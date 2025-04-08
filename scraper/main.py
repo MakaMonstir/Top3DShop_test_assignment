bc3799eb54f2e77983d99bb06386e050cacbee48
Timur Usmanov <fonuatimur@gmail.com>
2025-04-08 14:48:12 +0500

    add parser function

--- scraper/main.py
+++ scraper/main.py
@@ -1,13 +1,24 @@
 import os
 import time
+from typing import List
 
 import pandas as pd
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.remote.webelement import WebElement
 BASE_URL = "https://store.creality.com"
 SCANNERS_PATH = "/collections/scanners"
 PRODUCT_TAG_SELECTOR = "a.item-img"
 
+def find_all_elements_by_selector(
+    driver: WebDriver, base_url: str, path: str, selector: str
+) -> List[WebElement]:
+    print(base_url + path)
+    driver.get(base_url + path)
+    elements = driver.find_elements(By.CSS_SELECTOR, selector)
+    return elements
 
 def main():
     driver = get_driver()
